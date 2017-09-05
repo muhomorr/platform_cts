1010681da9d3afe89c775920800caf1fc583a5f2
Seigo Nonaka <nona@google.com>
2017-09-05 14:02:00 +0900

    Introduce a test case that the font has outside of Unicode code point. - DO NOT MERGE
    Bug: 62134807
    Test: mmma cts/tests/tests/graphics &&
          adb install -r $OUT/data/app/CtsGraphicsTestCases/CtsGraphicsTestCases.apk &&
          adb shell am instrument -w -m -e class \
          android.graphics.cts.TypefaceTest \
          android.graphics.cts/android.support.test.runner.AndroidJUnitRunner \
    
    Change-Id: Ie772a14b3e1857467e97bea18399bfd9f05cf98e
    (cherry picked from commit 42ecdf73facab1c8d11c2091eaad8834ef3ecaf3)
    (cherry picked from commit 825fe5d0b567d43ea531d24a605025d698f47e21)

--- tests/tests/graphics/src/android/graphics/cts/TypefaceTest.java
+++ tests/tests/graphics/src/android/graphics/cts/TypefaceTest.java
@@ -252,6 +252,22 @@ public class TypefaceTest {
     }
 
     @Test
+    public void testInvalidCmapFont_tooLargeCodePoints() {
+        // Following three font doen't have any coverage between U+0000..U+10FFFF. Just make sure
+        // they don't crash us.
+        final String[] INVALID_CMAP_FONTS = {
+            "out_of_unicode_start_cmap12.ttf",
+            "out_of_unicode_end_cmap12.ttf",
+            "too_large_start_cmap12.ttf",
+            "too_large_end_cmap12.ttf",
+        };
+        for (final String file : INVALID_CMAP_FONTS) {
+            final Typeface typeface = Typeface.createFromAsset(mContext.getAssets(), file);
+            assertNotNull(typeface);
+        }
+    }
+
+    @Test
     public void testCreateFromAsset_cachesTypeface() {
         Typeface typeface1 = Typeface.createFromAsset(mContext.getAssets(), "bombfont2.ttf");
         assertNotNull(typeface1);
