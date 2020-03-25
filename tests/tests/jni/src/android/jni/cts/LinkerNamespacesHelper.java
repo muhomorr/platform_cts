bluestacks/cts-a16

Fix JniStaticTest#test_linker_namespace on non-native (foreign) architecture on Android Q
Fix 2 problems when running foreign architecure
1. CTS test regression.
On foreign architecture, /system/lib/arm can be the default library
search path. But due to a CTS change, i.e. https://r.android.com/895418,
/system/lib/arm is disallowed to be on the "system path". That test
change was likely unintentional.
This change re-allow this library path.

2. Whitelist /system/lib/arm/libicuuc.so or libicui18n.so loadable by dlopen(3)
libicuuc.so and libicui18n.so in native architecture were moved into
/apex/com.android.runtime in Q. Later, the module is renamed into
com.android.art. But the foreign architecture ones stayed in
/system/lib/arm. Thus, whitelist the library here to allow the existing
behavior because there isn't a behavior change of loading
/system/lib/arm/libicuuc.so on non-native arch between Android P and Q+.

Verified: /system/lib/arm/libicuuc.so is loadable on P x86 device when
running on ARM mode.

Bug: 149852946
Test: cts-tradefed run cts -a armeabi-v7a -m CtsJniTestCases -t android.jni.cts.JniStaticTest
      The test fails on master because of neuralnetwork.so moved into APEX
Change-Id: Ib4600bd404dfb4b2a5cfabfcd115359e6ac441f5

tests/tests/jni/src/android/jni/cts/LinkerNamespacesHelper.java
@@ -19,6 +19,7 @@ package android.jni.cts;
 import android.content.pm.ApplicationInfo;
 import android.content.pm.PackageManager;
 import android.content.pm.PackageManager.NameNotFoundException;
+import android.os.Build;
 
 import androidx.test.InstrumentationRegistry;
 
@@ -187,6 +188,10 @@ class LinkerNamespacesHelper {
 
         Collections.addAll(systemLibs, PUBLIC_SYSTEM_LIBRARIES);
         Collections.addAll(systemLibs, OPTIONAL_SYSTEM_LIBRARIES);
+	// System path could contain public ART libraries on foreign arch. http://b/149852946
+        if (isForeignArchitecture()) {
+            Collections.addAll(systemLibs, PUBLIC_ART_LIBRARIES);
+        }
 
         if (InstrumentationRegistry.getContext().getPackageManager().
                 hasSystemFeature(PackageManager.FEATURE_WEBVIEW)) {
@@ -407,6 +412,22 @@ class LinkerNamespacesHelper {
     }
 
     public static native String tryDlopen(String lib);
+
+    private static boolean isForeignArchitecture() {
+        int libAbi = getLibAbi();
+        String cpuAbi = android.os.SystemProperties.get("ro.product.cpu.abi");
+        if ((libAbi == 1 || libAbi == 2) && !cpuAbi.startsWith("arm")) {
+            return true;
+        } else if ((libAbi == 3 || libAbi == 4) && !cpuAbi.startsWith("x86")) {
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * @return ABI type of the JNI library. 1: ARM64, 2:ARM, 3: x86_64, 4: x86, 0: others
+     */
+    private static native int getLibAbi();
 }
 
 class ClassNamespaceA1 {

muhomorr/platform_cts

Merged-In: Ib4600bd404dfb4b2a5cfabfcd115359e6ac441f5
(cherry picked from commit cf0bb73549cba53c67349af5c499d2dbceb00419)

tests/tests/jni/src/android/jni/cts/LinkerNamespacesHelper.java
@@ -19,6 +19,7 @@ package android.jni.cts;
 import android.content.pm.ApplicationInfo;
 import android.content.pm.PackageManager;
 import android.content.pm.PackageManager.NameNotFoundException;
+import android.os.Build;
 
 import androidx.test.InstrumentationRegistry;
 
@@ -157,6 +158,10 @@ class LinkerNamespacesHelper {
         List<String> runtimeApexLibs = new ArrayList<>();
 
         Collections.addAll(systemLibs, PUBLIC_SYSTEM_LIBRARIES);
+        // System path could contain public ART libraries on foreign arch. http://b/149852946
+        if (isForeignArchitecture()) {
+            Collections.addAll(systemLibs, PUBLIC_RUNTIME_LIBRARIES);
+        }
 
         if (InstrumentationRegistry.getContext().getPackageManager().
                 hasSystemFeature(PackageManager.FEATURE_WEBVIEW)) {
@@ -361,6 +366,22 @@ class LinkerNamespacesHelper {
     }
 
     public static native String tryDlopen(String lib);
+
+    private static boolean isForeignArchitecture() {
+        int libAbi = getLibAbi();
+        String cpuAbi = android.os.SystemProperties.get("ro.product.cpu.abi");
+        if ((libAbi == 1 || libAbi == 2) && !cpuAbi.startsWith("arm")) {
+            return true;
+        } else if ((libAbi == 3 || libAbi == 4) && !cpuAbi.startsWith("x86")) {
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * @return ABI type of the JNI library. 1: ARM64, 2:ARM, 3: x86_64, 4: x86, 0: others
+     */
+    private static native int getLibAbi();
 }
 
 class ClassNamespaceA1 {