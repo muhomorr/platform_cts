muhomorr/platform_cts

commit 6d168802e10b17510c3018755cac9ed0489a9cad

Don't assume latest SEPolicy.
This test will see one behavior if the vendor SEPolicy is S+ and
another if it is R- (even though we know the system version is S+). To
accommodate that, add an explicit check for the vendor build.

Bug: 169281003
Test: atest CtsAppSecurityHostTestCases:android.appsecurity.cts.AppDataIsolationTests
Change-Id: Ia9ed378e0b9ae5b4413f74aea978548a7a6b9857

hostsidetests/appsecurity/test-apps/AppDataIsolationTestApp/AppA/src/com/android/cts/appdataisolation/appa/IsolatedService.java
@@ -16,14 +16,18 @@
 
 package com.android.cts.appdataisolation.appa;
 
+import static com.android.cts.appdataisolation.common.FileUtils.assertDirDoesNotExist;
 import static com.android.cts.appdataisolation.common.FileUtils.assertDirIsNotAccessible;
 
 import android.app.Service;
 import android.content.Intent;
 import android.content.pm.ApplicationInfo;
+import android.os.Build;
 import android.os.IBinder;
 import android.os.RemoteException;
+import android.os.SystemProperties;
 
+import com.android.compatibility.common.util.PropertyUtil;
 import com.android.cts.appdataisolation.common.FileUtils;
 
 public class IsolatedService extends Service {
@@ -34,31 +38,73 @@ public class IsolatedService extends Service {
         public void assertDataIsolated() throws RemoteException {
             try {
                 ApplicationInfo applicationInfo = getApplicationInfo();
-                assertDirIsNotAccessible(applicationInfo.dataDir);
-                assertDirIsNotAccessible(applicationInfo.deviceProtectedDataDir);
-                assertDirIsNotAccessible("/data/data/" + getPackageName());
-                assertDirIsNotAccessible("/data/misc/profiles/cur/0/" + getPackageName());
+
                 assertDirIsNotAccessible("/data/misc/profiles/ref");
-
-                assertDirIsNotAccessible(FileUtils.replacePackageAWithPackageB(
-                        applicationInfo.dataDir));
-                assertDirIsNotAccessible(FileUtils.replacePackageAWithPackageB(
-                        applicationInfo.deviceProtectedDataDir));
-                assertDirIsNotAccessible("/data/data/" + FileUtils.APPB_PKG);
-                assertDirIsNotAccessible("/data/misc/profiles/cur/0/" + FileUtils.APPB_PKG);
-
-                assertDirIsNotAccessible(FileUtils.replacePackageAWithNotInstalledPkg(
-                        applicationInfo.dataDir));
-                assertDirIsNotAccessible(FileUtils.replacePackageAWithNotInstalledPkg(
-                        applicationInfo.deviceProtectedDataDir));
-                assertDirIsNotAccessible("/data/data/" + FileUtils.NOT_INSTALLED_PKG);
-                assertDirIsNotAccessible("/data/misc/profiles/cur/0/" + FileUtils.NOT_INSTALLED_PKG);
+                if (isVendorPolicyNewerThanR()) {
+                    // Restrictions are enforced in SELinux, so EACCESS is returned
+                    assertDirectoriesInaccessible(applicationInfo);
+                } else {
+                    // Vendor partition has older, more permissive SELinux policy.
+                    // Restrictions are enforced via app data isolation, so ENOENT is returned
+                    assertDirectoriesDoNotExist(applicationInfo);
+                }
             } catch (Throwable e) {
                 throw new IllegalStateException(e.getMessage());
             }
         }
 
     };
+
+    private boolean isVendorPolicyNewerThanR() {
+        if (SystemProperties.get("ro.vndk.version").equals("S")) {
+            // Vendor build is S, but before the API level bump - good enough for us.
+            return true;
+        }
+        return PropertyUtil.isVendorApiLevelNewerThan(Build.VERSION_CODES.R);
+    }
+
+    private void assertDirectoriesInaccessible(ApplicationInfo applicationInfo) {
+        assertDirIsNotAccessible(applicationInfo.dataDir);
+        assertDirIsNotAccessible(applicationInfo.deviceProtectedDataDir);
+        assertDirIsNotAccessible("/data/data/" + getPackageName());
+        assertDirIsNotAccessible("/data/misc/profiles/cur/0/" + getPackageName());
+
+        assertDirIsNotAccessible(FileUtils.replacePackageAWithPackageB(
+                applicationInfo.dataDir));
+        assertDirIsNotAccessible(FileUtils.replacePackageAWithPackageB(
+                applicationInfo.deviceProtectedDataDir));
+        assertDirIsNotAccessible("/data/data/" + FileUtils.APPB_PKG);
+        assertDirIsNotAccessible("/data/misc/profiles/cur/0/" + FileUtils.APPB_PKG);
+
+        assertDirIsNotAccessible(FileUtils.replacePackageAWithNotInstalledPkg(
+                applicationInfo.dataDir));
+        assertDirIsNotAccessible(FileUtils.replacePackageAWithNotInstalledPkg(
+                applicationInfo.deviceProtectedDataDir));
+        assertDirIsNotAccessible("/data/data/" + FileUtils.NOT_INSTALLED_PKG);
+        assertDirIsNotAccessible("/data/misc/profiles/cur/0/" + FileUtils.NOT_INSTALLED_PKG);
+    }
+
+    private void assertDirectoriesDoNotExist(ApplicationInfo applicationInfo) {
+        assertDirDoesNotExist(applicationInfo.dataDir);
+        assertDirDoesNotExist(applicationInfo.deviceProtectedDataDir);
+        assertDirDoesNotExist("/data/data/" + getPackageName());
+        assertDirDoesNotExist("/data/misc/profiles/cur/0/" + getPackageName());
+        assertDirDoesNotExist("/data/misc/profiles/ref");
+
+        assertDirDoesNotExist(FileUtils.replacePackageAWithPackageB(
+                applicationInfo.dataDir));
+        assertDirDoesNotExist(FileUtils.replacePackageAWithPackageB(
+                applicationInfo.deviceProtectedDataDir));
+        assertDirDoesNotExist("/data/data/" + FileUtils.APPB_PKG);
+        assertDirDoesNotExist("/data/misc/profiles/cur/0/" + FileUtils.APPB_PKG);
+
+        assertDirDoesNotExist(FileUtils.replacePackageAWithNotInstalledPkg(
+                applicationInfo.dataDir));
+        assertDirDoesNotExist(FileUtils.replacePackageAWithNotInstalledPkg(
+                applicationInfo.deviceProtectedDataDir));
+        assertDirDoesNotExist("/data/data/" + FileUtils.NOT_INSTALLED_PKG);
+        assertDirDoesNotExist("/data/misc/profiles/cur/0/" + FileUtils.NOT_INSTALLED_PKG);
+    }
 
     @Override
     public IBinder onBind(Intent intent) {

hostsidetests/appsecurity/test-apps/AppDataIsolationTestApp/Android.bp
@@ -16,8 +16,8 @@ android_test_helper_app {
     name: "CtsAppDataIsolationAppA",
     defaults: ["cts_support_defaults"],
     srcs: ["common/src/**/*.java", "AppA/src/**/*.java", "AppA/aidl/**/*.aidl"],
-    sdk_version: "current",
-    static_libs: ["androidx.test.rules", "truth-prebuilt", "testng", "ub-uiautomator"],
+    sdk_version: "test_current",
+    static_libs: ["androidx.test.rules", "truth-prebuilt", "testng", "ub-uiautomator", "compatibility-device-util-axt"],
     libs: ["android.test.base"],
     // tag this module as a cts test artifact
     test_suites: [
@@ -36,8 +36,8 @@ android_test_helper_app {
     name: "CtsAppDataIsolationAppSharedA",
     defaults: ["cts_support_defaults"],
     srcs: ["common/src/**/*.java", "AppA/src/**/*.java", "AppA/aidl/**/*.aidl"],
-    sdk_version: "current",
-    static_libs: ["androidx.test.rules", "truth-prebuilt", "testng", "ub-uiautomator"],
+    sdk_version: "test_current",
+    static_libs: ["androidx.test.rules", "truth-prebuilt", "testng", "ub-uiautomator", "compatibility-device-util-axt"],
     libs: ["android.test.base"],
     // tag this module as a cts test artifact
     test_suites: [
@@ -56,8 +56,8 @@ android_test_helper_app {
     name: "CtsAppDataIsolationAppDirectBootA",
     defaults: ["cts_support_defaults"],
     srcs: ["common/src/**/*.java", "AppA/src/**/*.java", "AppA/aidl/**/*.aidl"],
-    sdk_version: "current",
-    static_libs: ["androidx.test.rules", "truth-prebuilt", "testng", "ub-uiautomator"],
+    sdk_version: "test_current",
+    static_libs: ["androidx.test.rules", "truth-prebuilt", "testng", "ub-uiautomator", "compatibility-device-util-axt"],
     libs: ["android.test.base"],
     // tag this module as a cts test artifact
     test_suites: [
@@ -76,8 +76,8 @@ android_test_helper_app {
     name: "CtsAppDataIsolationAppB",
     defaults: ["cts_support_defaults"],
     srcs: ["common/src/**/*.java", "AppB/src/**/*.java"],
-    sdk_version: "current",
-    static_libs: ["androidx.test.rules", "truth-prebuilt", "testng"],
+    sdk_version: "test_current",
+    static_libs: ["androidx.test.rules", "truth-prebuilt", "testng", "ub-uiautomator", "compatibility-device-util-axt"],
     libs: ["android.test.base"],
     // tag this module as a cts test artifact
     test_suites: [
@@ -96,7 +96,7 @@ android_test_helper_app {
     name: "CtsAppDataIsolationAppSharedB",
     defaults: ["cts_support_defaults"],
     srcs: ["common/src/**/*.java", "AppB/src/**/*.java"],
-    sdk_version: "current",
+    sdk_version: "test_current",
     static_libs: ["androidx.test.rules", "truth-prebuilt", "testng"],
     libs: ["android.test.base"],
     // tag this module as a cts test artifact

commit 9e66e5a44b9eb33c53b1eea1290d4ca6a6f4155f

Fix test for /data/misc/profiles/ref.
This directory is inaccesible to apps and always has been. Commit
6d168802e10b17510c3018755cac9ed0489a9cad inadvertently added a test
that it would return ENOENT on a mixed GSI build, which is not the
case. (There is also a test that it returns EACCES, which passed.) So
we remove the bogus assertion.

Bug: 169281003
Test: atest CtsAppSecurityHostTestCases:android.appsecurity.cts.AppDataIsolationTests
Change-Id: I542b5a766e0d0d46232dd8f42c3017dea958c06b

hostsidetests/appsecurity/test-apps/AppDataIsolationTestApp/AppA/src/com/android/cts/appdataisolation/appa/IsolatedService.java
@@ -89,7 +89,6 @@ public class IsolatedService extends Service {
         assertDirDoesNotExist(applicationInfo.deviceProtectedDataDir);
         assertDirDoesNotExist("/data/data/" + getPackageName());
         assertDirDoesNotExist("/data/misc/profiles/cur/0/" + getPackageName());
-        assertDirDoesNotExist("/data/misc/profiles/ref");
 
         assertDirDoesNotExist(FileUtils.replacePackageAWithPackageB(
                 applicationInfo.dataDir));