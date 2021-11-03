bluestacks/cts-a16

Fixes test fail cause by unexpected events
In special case, there are some events, such as
{@link #TYPE_WINDOW_BOUNDS_CHANGED} and
{@link #TYPE_VIEW_INSETS_CHANGED}, will be accidentally
generated by the system. Because these events were not
expected in the test, remove them if needed.

Bug: 201610149
Fixes: 201322949
Fixes: 200998013
Fixes: 199785379
Fixes: 201006339
Fixes: 199785591
Fixes: 201028291
Test: atest CtsContentCaptureServiceTestCases
Change-Id: Ieaabc52157c8b2bba1c7d7646508b430888c1f02

tests/contentcaptureservice/src/android/contentcaptureservice/cts/Assertions.java
@@ -49,7 +49,9 @@ import androidx.annotation.Nullable;
 
 import com.android.compatibility.common.util.RetryableException;
 
+import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * Helper for common assertions.
@@ -267,7 +269,7 @@ final class Assertions {
         assertWithMessage("event %s (index %s) should not have a ViewNode", event, index)
                 .that(event.getViewNode()).isNull();
         assertWithMessage("event %s (index %s) should not have text", event, index)
-                .that(event.getViewNode()).isNull();
+                .that(event.getText()).isNull();
         assertWithMessage("event %s (index %s) should not have an autofillId", event, index)
                 .that(event.getId()).isNull();
         assertWithMessage("event %s (index %s) should not have autofillIds", event, index)
@@ -297,11 +299,29 @@ final class Assertions {
     public static void assertNoViewLevelEvents(@NonNull Session session,
             @NonNull AbstractContentCaptureActivity activity) {
         assertRightActivity(session, session.id, activity);
-        final List<ContentCaptureEvent> events = session.getEvents();
+        final List<ContentCaptureEvent> events = removeUnexpectedEvents(session.getEvents());
         Log.v(TAG, "events on " + activity + ": " + events);
         assertThat(events).hasSize(2);
         assertSessionResumed(events, 0);
         assertSessionPaused(events, 1);
+    }
+
+    /**
+     * This method is used to remove unexpected events if the test should only
+     * contain session level events.
+     * In special case, there are some events, such as {@link #TYPE_WINDOW_BOUNDS_CHANGED}
+     * and {@link #TYPE_VIEW_INSETS_CHANGED}, will be accidentally generated by
+     * the system. Because these events were not expected in the test, remove
+     * them if needed.
+     */
+    public static List<ContentCaptureEvent> removeUnexpectedEvents(
+            @NonNull List<ContentCaptureEvent> events) {
+        return Collections.unmodifiableList(events).stream().filter(
+                e -> e.getType() != TYPE_WINDOW_BOUNDS_CHANGED
+                        && e.getType() != TYPE_VIEW_INSETS_CHANGED
+                        && e.getType() != TYPE_VIEW_TREE_APPEARING
+                        && e.getType() != TYPE_VIEW_TREE_APPEARED
+        ).collect(Collectors.toList());
     }
 
     /**

tests/contentcaptureservice/src/android/contentcaptureservice/cts/ChildlessActivityTest.java
@@ -30,6 +30,7 @@ import static android.contentcaptureservice.cts.Assertions.assertViewDisappeared
 import static android.contentcaptureservice.cts.Assertions.assertViewTreeFinished;
 import static android.contentcaptureservice.cts.Assertions.assertViewTreeStarted;
 import static android.contentcaptureservice.cts.Assertions.assertViewsDisappeared;
+import static android.contentcaptureservice.cts.Assertions.removeUnexpectedEvents;
 import static android.contentcaptureservice.cts.Helper.newImportantView;
 import static android.contentcaptureservice.cts.Helper.sContext;
 
@@ -177,7 +178,6 @@ public class ChildlessActivityTest
         activity2.assertDefaultEvents(session2);
     }
 
-
     @Test
     public void testLaunchAnotherActivity_onTopOfIt() throws Exception {
         final CtsContentCaptureService service = enableService();
@@ -208,10 +208,8 @@ public class ChildlessActivityTest
             .activityResumed(name1)
             .activityPaused(name1)
             .activityResumed(name2)
-            .activityStopped(name1)
             .activityPaused(name2)
             .activityResumed(name1)
-            .activityDestroyed(name2)
             .activityPaused(name1);
 
         // Assert the sessions
@@ -223,7 +221,7 @@ public class ChildlessActivityTest
         Log.v(TAG, "session id2: " + sessionId2);
 
         final Session session1 = service.getFinishedSession(sessionId1);
-        final List<ContentCaptureEvent> events1 = session1.getEvents();
+        final List<ContentCaptureEvent> events1 = removeUnexpectedEvents(session1.getEvents());
         Log.v(TAG, "events on " + activity1 + ": " + events1);
         assertThat(events1).hasSize(4);
         assertSessionResumed(events1, 0);

muhomorr/platform_cts

Merged-In: Ieaabc52157c8b2bba1c7d7646508b430888c1f02
(cherry picked from commit a67114b441419951ebfe3ba3d214dceaff989423)

tests/contentcaptureservice/src/android/contentcaptureservice/cts/Assertions.java
@@ -49,7 +49,9 @@ import androidx.annotation.Nullable;
 
 import com.android.compatibility.common.util.RetryableException;
 
+import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * Helper for common assertions.
@@ -267,7 +269,7 @@ final class Assertions {
         assertWithMessage("event %s (index %s) should not have a ViewNode", event, index)
                 .that(event.getViewNode()).isNull();
         assertWithMessage("event %s (index %s) should not have text", event, index)
-                .that(event.getViewNode()).isNull();
+                .that(event.getText()).isNull();
         assertWithMessage("event %s (index %s) should not have an autofillId", event, index)
                 .that(event.getId()).isNull();
         assertWithMessage("event %s (index %s) should not have autofillIds", event, index)
@@ -297,11 +299,29 @@ final class Assertions {
     public static void assertNoViewLevelEvents(@NonNull Session session,
             @NonNull AbstractContentCaptureActivity activity) {
         assertRightActivity(session, session.id, activity);
-        final List<ContentCaptureEvent> events = session.getEvents();
+        final List<ContentCaptureEvent> events = removeBoundsAndInsetsEvents(session.getEvents());
         Log.v(TAG, "events on " + activity + ": " + events);
         assertThat(events).hasSize(2);
         assertSessionResumed(events, 0);
         assertSessionPaused(events, 1);
+    }
+
+    /**
+     * This method is used to remove Bounds and Insets changed events if the test should only
+     * contain session level events.
+     * In special case, there are some events, such as {@link #TYPE_WINDOW_BOUNDS_CHANGED}
+     * and {@link #TYPE_VIEW_INSETS_CHANGED}, will be accidentally generated by
+     * the system. Because these events were not expected in the test, remove
+     * them if needed.
+     */
+    public static List<ContentCaptureEvent> removeBoundsAndInsetsEvents(
+            @NonNull List<ContentCaptureEvent> events) {
+        return Collections.unmodifiableList(events).stream().filter(
+                e -> e.getType() != TYPE_WINDOW_BOUNDS_CHANGED
+                        && e.getType() != TYPE_VIEW_INSETS_CHANGED
+                        && e.getType() != TYPE_VIEW_TREE_APPEARING
+                        && e.getType() != TYPE_VIEW_TREE_APPEARED
+        ).collect(Collectors.toList());
     }
 
     /**

tests/contentcaptureservice/src/android/contentcaptureservice/cts/ChildlessActivityTest.java
@@ -30,6 +30,7 @@ import static android.contentcaptureservice.cts.Assertions.assertViewDisappeared
 import static android.contentcaptureservice.cts.Assertions.assertViewTreeFinished;
 import static android.contentcaptureservice.cts.Assertions.assertViewTreeStarted;
 import static android.contentcaptureservice.cts.Assertions.assertViewsDisappeared;
+import static android.contentcaptureservice.cts.Assertions.removeBoundsAndInsetsEvents;
 import static android.contentcaptureservice.cts.Helper.newImportantView;
 import static android.contentcaptureservice.cts.Helper.sContext;
 
@@ -177,7 +178,6 @@ public class ChildlessActivityTest
         activity2.assertDefaultEvents(session2);
     }
 
-
     @Test
     public void testLaunchAnotherActivity_onTopOfIt() throws Exception {
         final CtsContentCaptureService service = enableService();
@@ -208,10 +208,8 @@ public class ChildlessActivityTest
             .activityResumed(name1)
             .activityPaused(name1)
             .activityResumed(name2)
-            .activityStopped(name1)
             .activityPaused(name2)
             .activityResumed(name1)
-            .activityDestroyed(name2)
             .activityPaused(name1);
 
         // Assert the sessions
@@ -223,7 +221,7 @@ public class ChildlessActivityTest
         Log.v(TAG, "session id2: " + sessionId2);
 
         final Session session1 = service.getFinishedSession(sessionId1);
-        final List<ContentCaptureEvent> events1 = session1.getEvents();
+        final List<ContentCaptureEvent> events1 = removeBoundsAndInsetsEvents(session1.getEvents());
         Log.v(TAG, "events on " + activity1 + ": " + events1);
         assertThat(events1).hasSize(4);
         assertSessionResumed(events1, 0);