bluestacks/cts-a16

Support Hsum for RoR CTS.
* Do not attempt to switch to the headless user (user 0)
* Check service interaction as user 0 since RoR runs as system user.
* Modify runDeviceTests as user to actually run as provided user instead of running as current user (this revealed a few bugs described below)
* Fix bug in resumeOnReboot_TwoUsers_BothUserUnlock_Success that never checks primary user is unlocked
* Fix unnecessary second call to verifyUnlockedAndDismiss resumeOnReboot_ManagedProfile_Success

Test: atest -c CtsAppSecurityHostTestCases:ResumeOnRebootHostTest
with/without hsum

Bug: 284081769
(cherry picked from https://googleplex-android-review.googlesource.com/q/commit:8b86b1850a6826eb6c89068d66ab61ec9fdcae82)
Change-Id: I61bde3f06c06b454c4a57fe20d86a95fac74a425

hostsidetests/appsecurity/src/android/appsecurity/cts/ResumeOnRebootHostTest.java
@@ -42,6 +42,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.concurrent.TimeUnit;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -127,7 +128,7 @@ public class ResumeOnRebootHostTest extends BaseHostJUnit4Test {
             return;
         }
 
-        int[] users = Utils.prepareSingleUser(getDevice());
+        int[] users = prepareUsers(1);
         int initialUser = users[0];
 
         int managedUserId = createManagedProfile(initialUser);
@@ -143,7 +144,6 @@ public class ResumeOnRebootHostTest extends BaseHostJUnit4Test {
             deviceRebootAndApply();
 
             runDeviceTestsAsUser("testVerifyUnlockedAndDismiss", initialUser);
-            runDeviceTestsAsUser("testVerifyUnlockedAndDismiss", managedUserId);
         } finally {
             stopUserAsync(managedUserId);
             removeUser(managedUserId);
@@ -165,7 +165,7 @@ public class ResumeOnRebootHostTest extends BaseHostJUnit4Test {
             return;
         }
 
-        int[] users = Utils.prepareMultipleUsers(getDevice(), 2);
+        int[] users = prepareUsers(2);
         int initialUser = users[0];
         int secondaryUser = users[1];
 
@@ -189,6 +189,7 @@ public class ResumeOnRebootHostTest extends BaseHostJUnit4Test {
             // Try to start early to calm down broadcast storms.
             getDevice().startUser(secondaryUser);
 
+            switchUser(initialUser);
             runDeviceTestsAsUser("testVerifyUnlockedAndDismiss", initialUser);
 
             switchUser(secondaryUser);
@@ -214,7 +215,7 @@ public class ResumeOnRebootHostTest extends BaseHostJUnit4Test {
             return;
         }
 
-        int[] users = Utils.prepareMultipleUsers(getDevice(), 2);
+        int[] users = prepareUsers(2);
         int initialUser = users[0];
         int secondaryUser = users[1];
 
@@ -237,13 +238,11 @@ public class ResumeOnRebootHostTest extends BaseHostJUnit4Test {
 
             deviceRebootAndApply();
 
-            // Try to start early to calm down broadcast storms.
-            getDevice().startUser(secondaryUser);
-
-            runDeviceTestsAsUser("testVerifyUnlockedAndDismiss", initialUser);
-
             switchUser(secondaryUser);
             runDeviceTestsAsUser("testVerifyUnlockedAndDismiss", secondaryUser);
+
+            switchUser(initialUser);
+            runDeviceTestsAsUser("testVerifyUnlockedAndDismiss", initialUser);
         } finally {
             // Remove secure lock screens and tear down test app
             switchUser(secondaryUser);
@@ -260,7 +259,7 @@ public class ResumeOnRebootHostTest extends BaseHostJUnit4Test {
         assumeTrue("Device isn't at least S or has no lock screen", isSupportedSDevice());
         assumeTrue("Device does not support file-based encryption", supportFileBasedEncryption());
 
-        int[] users = Utils.prepareSingleUser(getDevice());
+        int[] users = prepareUsers(1);
         int initialUser = users[0];
 
         try {
@@ -273,7 +272,8 @@ public class ResumeOnRebootHostTest extends BaseHostJUnit4Test {
             deviceRebootAndApply();
 
             runDeviceTestsAsUser("testVerifyUnlockedAndDismiss", initialUser);
-            runDeviceTestsAsUser("testCheckServiceInteraction", initialUser);
+            // Check service interaction as user 0 since RoR always runs as system user.
+            runDeviceTestsAsUser("testCheckServiceInteraction", /* userId= */ 0);
         } finally {
             // Remove secure lock screens and tear down test app
             runDeviceTestsAsUser("testTearDown", initialUser);
@@ -287,7 +287,7 @@ public class ResumeOnRebootHostTest extends BaseHostJUnit4Test {
         assumeTrue("Device isn't at least S or has no lock screen", isSupportedSDevice());
         assumeTrue("Device does not support file-based encryption", supportFileBasedEncryption());
 
-        int[] users = Utils.prepareSingleUser(getDevice());
+        int[] users = prepareUsers(1);
         int initialUser = users[0];
 
         final String clientA = "ClientA";
@@ -307,7 +307,8 @@ public class ResumeOnRebootHostTest extends BaseHostJUnit4Test {
             deviceRebootAndApply(clientA);
 
             runDeviceTestsAsUser("testVerifyUnlockedAndDismiss", initialUser);
-            runDeviceTestsAsUser("testCheckServiceInteraction", initialUser);
+            // Check service interaction as user 0 since RoR always runs as system user.
+            runDeviceTestsAsUser("testCheckServiceInteraction", /* userId= */ 0);
         } finally {
             // Remove secure lock screens and tear down test app
             runDeviceTestsAsUser("testTearDown", initialUser);
@@ -321,7 +322,7 @@ public class ResumeOnRebootHostTest extends BaseHostJUnit4Test {
         assumeTrue("Device isn't at least S or has no lock screen", isSupportedSDevice());
         assumeTrue("Device does not support file-based encryption", supportFileBasedEncryption());
 
-        int[] users = Utils.prepareSingleUser(getDevice());
+        int[] users = prepareUsers(1);
         int initialUser = users[0];
 
         final String clientA = "ClientA";
@@ -340,7 +341,8 @@ public class ResumeOnRebootHostTest extends BaseHostJUnit4Test {
             deviceRebootAndApply(clientB);
 
             runDeviceTestsAsUser("testVerifyUnlockedAndDismiss", initialUser);
-            runDeviceTestsAsUser("testCheckServiceInteraction", initialUser);
+            // Check service interaction as user 0 since RoR always runs as system user.
+            runDeviceTestsAsUser("testCheckServiceInteraction", /* userId= */ 0);
         } finally {
             // Remove secure lock screens and tear down test app
             runDeviceTestsAsUser("testTearDown", initialUser);
@@ -513,7 +515,8 @@ public class ResumeOnRebootHostTest extends BaseHostJUnit4Test {
     }
 
     private void removeUser(int userId) throws Exception {
-        if (listUsers().contains(userId) && userId != USER_SYSTEM) {
+        if (listUsers().contains(userId) && userId != USER_SYSTEM
+                && userId != getDevice().getPrimaryUserId()) {
             // Don't log output, as tests sometimes set no debug user restriction, which
             // causes this to fail, we should still continue and remove the user.
             String stopUserCommand = "am stop-user -w -f " + userId;
@@ -553,7 +556,7 @@ public class ResumeOnRebootHostTest extends BaseHostJUnit4Test {
 
     private void runDeviceTestsAsUser(String testMethodName, int userId)
             throws DeviceNotAvailableException {
-        Utils.runDeviceTestsAsCurrentUser(getDevice(), PKG, CLASS, testMethodName);
+        Utils.runDeviceTests(getDevice(), PKG, CLASS, testMethodName, userId);
     }
 
     private boolean isSupportedSDevice() throws Exception {
@@ -617,5 +620,15 @@ public class ResumeOnRebootHostTest extends BaseHostJUnit4Test {
         String result = getDevice().executeShellCommand(command);
         CLog.d("Output for command \"" + command + "\": " + result);
         return result;
+    }
+
+    private int[] prepareUsers(int users) throws DeviceNotAvailableException {
+        if (getDevice().isHeadlessSystemUserMode()) {
+            // Prepare an additional user to account for headless system user.
+            int[] ids = Utils.prepareMultipleUsers(getDevice(), users + 1);
+            // Do not return system user since it does not have full user capabilities.
+            return Arrays.copyOfRange(ids, 1, ids.length);
+        }
+        return Utils.prepareMultipleUsers(getDevice(), users);
     }
 }

muhomorr/platform_cts

(cherry picked from https://googleplex-android-review.googlesource.com/q/commit:c73c0695055960a2739965be419d72baee8f7603)
Merged-In: I61bde3f06c06b454c4a57fe20d86a95fac74a425

hostsidetests/appsecurity/src/android/appsecurity/cts/ResumeOnRebootHostTest.java
@@ -42,6 +42,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.concurrent.TimeUnit;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -127,7 +128,7 @@ public class ResumeOnRebootHostTest extends BaseHostJUnit4Test {
             return;
         }
 
-        int[] users = Utils.prepareSingleUser(getDevice());
+        int[] users = prepareUsers(1);
         int initialUser = users[0];
 
         int managedUserId = createManagedProfile(initialUser);
@@ -143,7 +144,6 @@ public class ResumeOnRebootHostTest extends BaseHostJUnit4Test {
             deviceRebootAndApply();
 
             runDeviceTestsAsUser("testVerifyUnlockedAndDismiss", initialUser);
-            runDeviceTestsAsUser("testVerifyUnlockedAndDismiss", managedUserId);
         } finally {
             stopUserAsync(managedUserId);
             removeUser(managedUserId);
@@ -165,7 +165,7 @@ public class ResumeOnRebootHostTest extends BaseHostJUnit4Test {
             return;
         }
 
-        int[] users = Utils.prepareMultipleUsers(getDevice(), 2);
+        int[] users = prepareUsers(2);
         int initialUser = users[0];
         int secondaryUser = users[1];
 
@@ -189,6 +189,7 @@ public class ResumeOnRebootHostTest extends BaseHostJUnit4Test {
             // Try to start early to calm down broadcast storms.
             getDevice().startUser(secondaryUser);
 
+            switchUser(initialUser);
             runDeviceTestsAsUser("testVerifyUnlockedAndDismiss", initialUser);
 
             switchUser(secondaryUser);
@@ -214,7 +215,7 @@ public class ResumeOnRebootHostTest extends BaseHostJUnit4Test {
             return;
         }
 
-        int[] users = Utils.prepareMultipleUsers(getDevice(), 2);
+        int[] users = prepareUsers(2);
         int initialUser = users[0];
         int secondaryUser = users[1];
 
@@ -240,10 +241,11 @@ public class ResumeOnRebootHostTest extends BaseHostJUnit4Test {
             // Try to start early to calm down broadcast storms.
             getDevice().startUser(secondaryUser);
 
+            switchUser(initialUser);
             runDeviceTestsAsUser("testVerifyUnlockedAndDismiss", initialUser);
 
             switchUser(secondaryUser);
-            runDeviceTestsAsUser("testVerifyUnlockedAndDismiss", secondaryUser);
+            runDeviceTestsAsUser("testVerifyLockedAndDismiss", secondaryUser);
         } finally {
             // Remove secure lock screens and tear down test app
             switchUser(secondaryUser);
@@ -260,7 +262,7 @@ public class ResumeOnRebootHostTest extends BaseHostJUnit4Test {
         assumeTrue("Device isn't at least S or has no lock screen", isSupportedSDevice());
         assumeTrue("Device does not support file-based encryption", supportFileBasedEncryption());
 
-        int[] users = Utils.prepareSingleUser(getDevice());
+        int[] users = prepareUsers(1);
         int initialUser = users[0];
 
         try {
@@ -273,7 +275,8 @@ public class ResumeOnRebootHostTest extends BaseHostJUnit4Test {
             deviceRebootAndApply();
 
             runDeviceTestsAsUser("testVerifyUnlockedAndDismiss", initialUser);
-            runDeviceTestsAsUser("testCheckServiceInteraction", initialUser);
+            // Check service interaction as user 0 since RoR always runs as system user.
+            runDeviceTestsAsUser("testCheckServiceInteraction", /* userId= */ 0);
         } finally {
             // Remove secure lock screens and tear down test app
             runDeviceTestsAsUser("testTearDown", initialUser);
@@ -287,7 +290,7 @@ public class ResumeOnRebootHostTest extends BaseHostJUnit4Test {
         assumeTrue("Device isn't at least S or has no lock screen", isSupportedSDevice());
         assumeTrue("Device does not support file-based encryption", supportFileBasedEncryption());
 
-        int[] users = Utils.prepareSingleUser(getDevice());
+        int[] users = prepareUsers(1);
         int initialUser = users[0];
 
         final String clientA = "ClientA";
@@ -307,7 +310,8 @@ public class ResumeOnRebootHostTest extends BaseHostJUnit4Test {
             deviceRebootAndApply(clientA);
 
             runDeviceTestsAsUser("testVerifyUnlockedAndDismiss", initialUser);
-            runDeviceTestsAsUser("testCheckServiceInteraction", initialUser);
+            // Check service interaction as user 0 since RoR always runs as system user.
+            runDeviceTestsAsUser("testCheckServiceInteraction", /* userId= */ 0);
         } finally {
             // Remove secure lock screens and tear down test app
             runDeviceTestsAsUser("testTearDown", initialUser);
@@ -321,7 +325,7 @@ public class ResumeOnRebootHostTest extends BaseHostJUnit4Test {
         assumeTrue("Device isn't at least S or has no lock screen", isSupportedSDevice());
         assumeTrue("Device does not support file-based encryption", supportFileBasedEncryption());
 
-        int[] users = Utils.prepareSingleUser(getDevice());
+        int[] users = prepareUsers(1);
         int initialUser = users[0];
 
         final String clientA = "ClientA";
@@ -340,7 +344,8 @@ public class ResumeOnRebootHostTest extends BaseHostJUnit4Test {
             deviceRebootAndApply(clientB);
 
             runDeviceTestsAsUser("testVerifyUnlockedAndDismiss", initialUser);
-            runDeviceTestsAsUser("testCheckServiceInteraction", initialUser);
+            // Check service interaction as user 0 since RoR always runs as system user.
+            runDeviceTestsAsUser("testCheckServiceInteraction", /* userId= */ 0);
         } finally {
             // Remove secure lock screens and tear down test app
             runDeviceTestsAsUser("testTearDown", initialUser);
@@ -513,7 +518,8 @@ public class ResumeOnRebootHostTest extends BaseHostJUnit4Test {
     }
 
     private void removeUser(int userId) throws Exception {
-        if (listUsers().contains(userId) && userId != USER_SYSTEM) {
+        if (listUsers().contains(userId) && userId != USER_SYSTEM
+                && userId != getDevice().getMainUserId()) {
             // Don't log output, as tests sometimes set no debug user restriction, which
             // causes this to fail, we should still continue and remove the user.
             String stopUserCommand = "am stop-user -w -f " + userId;
@@ -553,7 +559,7 @@ public class ResumeOnRebootHostTest extends BaseHostJUnit4Test {
 
     private void runDeviceTestsAsUser(String testMethodName, int userId)
             throws DeviceNotAvailableException {
-        Utils.runDeviceTestsAsCurrentUser(getDevice(), PKG, CLASS, testMethodName);
+        Utils.runDeviceTests(getDevice(), PKG, CLASS, testMethodName, userId);
     }
 
     private boolean isSupportedSDevice() throws Exception {
@@ -617,5 +623,15 @@ public class ResumeOnRebootHostTest extends BaseHostJUnit4Test {
         String result = getDevice().executeShellCommand(command);
         CLog.d("Output for command \"" + command + "\": " + result);
         return result;
+    }
+
+    private int[] prepareUsers(int users) throws DeviceNotAvailableException {
+        if (getDevice().isHeadlessSystemUserMode()) {
+            // Prepare an additional user to account for headless system user.
+            int[] ids = Utils.prepareMultipleUsers(getDevice(), users + 1);
+            // Do not return system user since it does not have full user capabilities.
+            return Arrays.copyOfRange(ids, 1, ids.length);
+        }
+        return Utils.prepareMultipleUsers(getDevice(), users);
     }
 }